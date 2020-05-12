8cb33df864454f40ada7c5c080cee438469ed0a0
Kevin <kevin.bougis@gmail.com>
2020-05-12 15:24:52 +0200

    KevinController

--- src/main/java/flore/model/Views.java
+++ src/main/java/flore/model/Views.java
@@ -4,4 +4,14 @@ public class Views {
 	public static class ViewCommon {}
 	
 	public static class ViewCaracteristique extends ViewCommon {}
+	
+	public static class ViewFaune extends ViewCommon {}
+	
+	public static class ViewReferentielFaune extends ViewCommon {}
+	
+	public static class ViewUtilisateur extends ViewCommon {}
+	
+	public static class ViewReferentielUtilisateur extends ViewCommon {}
+	
+	public static class ViewSelection extends ViewCommon {}
 }
